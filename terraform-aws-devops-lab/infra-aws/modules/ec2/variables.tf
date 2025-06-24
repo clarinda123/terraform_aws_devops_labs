bb54ed137e4856e2bad1790ee70b8d5561dc18a0
clarinda123 <144284548+clarinda123@users.noreply.github.com>
2025-06-24 00:07:56 +0000

    Delete terraform-aws-devops-lab directory

--- terraform-aws-devops-lab/infra-aws/modules/ec2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ami" { type = string }
-variable "instance_type" { type = string }
-variable "subnet_id" { type = string }
-variable "security_group_id" { type = string }
-variable "user_data_path" { type = string }
-variable "instance_name" { type = string }
-variable "key_name" { type = string}
-
